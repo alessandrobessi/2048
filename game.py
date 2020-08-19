772415a009a215b87b3cc5c8c4e9a6fe686cfd78
Alessandro Bessi <abessi@nextbit.it>
2020-08-19 16:48:56 +0200

    simpler code

--- game.py
+++ game.py
@@ -1,18 +1,20 @@
 import random
+from tqdm import tqdm
+
 from board import Board
 
 if __name__ == "__main__":
     b = Board()
     moves = ["UP", "DOWN", "RIGHT", "LEFT"]
-    for _ in range(1000):
+    num_moves = 0
+    while not b.is_game_over():
         moved = b.move(random.choice(moves))
 
         if moved and not b.is_game_over():
+            num_moves += 1
             b.fill_cell()
-            continue
 
-        if b.is_game_over():
-            print("GAME OVER!")
-            print("score: ", b.score)
-            print(b.board)
-            break
+    print("GAME OVER!")
+    print("score: ", b.score)
+    print("num moves: ", num_moves)
+    print(b.board)
